8ceafcd922202219047d0bf15f6f742a735f8344
tiger2075 <tigercao@7senseslabs.com>
2017-11-09 09:36:19 +0800

    topic select list created

--- js/main.js
+++ js/main.js
@@ -73,7 +73,14 @@ function createWebSocket(){
 
 function disconnect(){
     if(rbServer){
-        rbServer.close()
+        rbServer.close();
+
+        if(poseTopic){
+            poseTopic.unsubscribe(function(){
+                console.log('poseTopic is unsubscribed...')
+            });
+        }
+
         var connectBtn = document.getElementById('connect');
         connectBtn.className = 'btn btn-warning';
     }
